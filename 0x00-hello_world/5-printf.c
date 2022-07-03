4533ae5ccdfe8b9b8e1a0f78ee3d9a5b7203153c
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-03 22:01:36 +0000

    Use the printf command

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Returns: zero for successful executtion
+ */
+
+int main (void)
+{
+	char *sent = "with proper grammar, but the outcome is a piece of art,\n";
+
+	printf(sent);
+	return (0);
+}
