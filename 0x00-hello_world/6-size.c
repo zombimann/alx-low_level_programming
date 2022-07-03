f1bc469ab91aada5ea17234cd549048e6da10b2e
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-03 23:20:47 +0000

    Find the sizes of the various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Zero for successful operation
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d bytes(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
+}
