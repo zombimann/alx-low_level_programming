2e23574d3b505c8409c08a1e0e04b0f070c972fa
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-03 22:05:52 +0000

    Use the printf command

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Returns: zero for successful executtion
+ * Return: zero for successful executtion
  */
 
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
