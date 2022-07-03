193b4219f21b879ce5c478bfed3ee07d0b8dd92b
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-03 21:49:17 +0000

    output a sentense

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ * 
+ * Return: zero for successful operation
+ */
+
+int main(void)
+{
+	char *sentence;
+	sentence = "Programming is like building a multilingual puzzle";
+
+	puts(sentence);
+	return (0);
+}
