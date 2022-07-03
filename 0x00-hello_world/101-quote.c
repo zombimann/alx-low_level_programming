e1f1f8ad60335392a872694891c51cb6f7875da3
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-03 23:34:01 +0000

    Print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 1 since it print to stderr
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
