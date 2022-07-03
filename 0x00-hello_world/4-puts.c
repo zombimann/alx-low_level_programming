b35fe85d7462ba33967467049d1baad4e4dc2bec
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-03 21:56:41 +0000

    Dot the tees

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
- * 
+ *
  * Return: zero for successful operation
  */
 
 int main(void)
 {
 	char *sentence;
+
 	sentence = "\"Programming is like building a multilingual puzzle";
 
 	puts(sentence);
