dcf21cb737c75186c71c1803a01f7ef80d0abcf1
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-04 22:45:30 +0000

    Print the single digit integers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Zero for successful execution
+ */
+
+int main (void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
