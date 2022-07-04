21ab8a7af5e82e88a0a473cd6bc3381eef7e1bc6
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-04 07:56:50 +0000

    Remove typo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	rem = abs(n % 10);
+	rem = n % 10;
 	if (rem > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	else if (rem == 0)
