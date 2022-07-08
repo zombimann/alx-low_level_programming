7082dd0c8a619944a7a48676a5ae4699b5176ebf
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-08 23:31:40 +0000

    Make some changes

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,10 +13,10 @@ void print_most_numbers(void)
 
 	for (i = '0'; i < '9' + 1; i++)
 	{
-		if ( i != ('2' || '4'))
-			{
-				_putchar(i);
-			}
+		if ( i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
