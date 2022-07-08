7e774e224707b36a4da647d83e8d058eaee6e304
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-08 10:49:46 +0000

    upload the code

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - Prints the digits from 0 to 9 using putchar
+ *
+ * Return: Nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = '0', i <= '9', i++)
+		putchar('i');
+	putchar('\n');
+}
