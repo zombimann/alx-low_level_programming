2a5081d894aeac566e8c3d516b18f74e50185893
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-08 10:57:41 +0000

    Include some missing files

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -11,6 +11,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = '0', i <= '9', i++)
-		putchar('i');
-	putchar('\n');
+		_putchar('i');
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _isupper(char c);
 int _isdigit(char c);
 int mul(int a, int b);
 void print_numbers(void);
+int _putchar(char c);
