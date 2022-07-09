bef6ef3206a15aa0994b1e5fa0142fde7980c53b
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-09 00:52:37 +0000

    Upload fixes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,13 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i = 9 + 1; i++)
+	for (i = 0; i < 9 + 1; i++)
 	{
 		for (j = 0; j < 14 + 1; j++)
 		{
-			_putchar((i/10) + '0');
-			_putchar((i%10) + '0');
+			if (j > 9)
+				_putchar((i/10) + '1');
+			_putchar((j%10) + '0');
 		}
 		_putchar('\n');
 	}
