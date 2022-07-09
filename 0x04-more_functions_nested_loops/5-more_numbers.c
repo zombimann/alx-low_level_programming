3cf8533f874be6441140784e96bc8a2ddc39c362
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-09 00:34:49 +0000

    Upload the program

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - print the numbers 0 to 14 ten times
+ *
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i = 9 + 1; i++)
+	{
+		for (j = 0; j < 14 + 1; j++)
+		{
+			_putchar((i/10) + '0');
+			_putchar((i%10) + '0');
+		}
+		_putchar('\n');
+	}
+}
