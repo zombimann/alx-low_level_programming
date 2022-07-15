b0932a3cdbdae24577940c72849524bacccb2d00
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-15 19:24:49 +0000

    Try it out

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,16 @@ void print_rev(char *s)
 	int n;
 
 	n = 0;
-	while(*s++ != '\0')
+	while (*s != '\0')
+	{
+		s++;
 		n++;
-	while(n-- > 0)
-		putchar (*s--);
+	}
+	while (n >= 0)
+	{
+		putchar (*s);
+		n--;
+		s--;
+	}
 	putchar ('\n');
 }
