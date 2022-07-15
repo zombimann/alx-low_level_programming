fd274b5dcb258cc94a302e2d782db6703e0cc202
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-15 14:23:55 +0000

    Make fixes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,11 +13,11 @@ int print_last_digit(int n)
 	if (n > 0)
 	{
 		_putchar ('0' + (n % 10));
-		return (abs(n) % 10);
+		return (n % 10);
 	}
 	else
 	{
 		_putchar ('0' + (-1*(n) % 10));
-		return (abs(n) % 10);
+		return (-1*(n) % 10);
 	}
 }
