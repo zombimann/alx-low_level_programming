1c765381e524b0a148dbb8306ae3e4ad9a2927fa
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-15 22:29:09 +0000

    Upload

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers the \n
+ * @a: The array of integers
+ * @n: Number of elements to be printed
+ *
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	
