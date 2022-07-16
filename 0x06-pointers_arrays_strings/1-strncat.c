dcb47efc4e23b518ab08d332db98f482c0d544a2
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-16 02:23:06 +0000

    upload

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * _strncat - appends src string to the dest string, overwriting ther terminal null and adding a terminal null
+ * _strncat - appends src string to the dest string
  * @dest: The string whose length is to be added
  * @src: The string to be appended to the dest string
  * @n: Length of tring to be appended
@@ -14,10 +14,19 @@ char *_strncat(char *dest, char *src, int n)
 
 	i = j = 0;
 
-	while(*dest++ != '\0')
-		i++;
-	while(src[j] != '\0' && j < n)
-		dest[i++] = src[j++];
+	while (*dest != '\0')
+        {
+                dest++;
+                i++;
+        }
+        while (*src != '\0' && j++ < n )
+        {
+                i++;
+                *dest++ = *src++;
+        }
+
+        while (i-- > 0)
+                dest--;
 
 	return (dest);
 }
