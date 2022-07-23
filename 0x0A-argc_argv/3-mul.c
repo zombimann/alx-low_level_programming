911d433ab1378b1cfdc045d3106d813319dba296
Mugambi Ndwiga <mugambindwiga@gmail.com>
2022-07-23 18:01:13 +0000

    upload

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name followed by a newline
+ * @argc: the number of arguments
+ * @argv: the character array of pointers to the arguments
+ *
+ * Return: 0 for successful execution
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}
+
